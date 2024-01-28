60093e09dcd3b642da2448d24926fdacefd03ffd
JABSOL <119754639+JABSOL@users.noreply.github.com>
2024-01-28 16:48:32 +0100

    11

--- ripple.js
+++ ripple.js
@@ -1,26 +1,13 @@
-[effect=ripple] {
-    cursor: pointer;
-    overflow: hidden;
-    position: relative;
-    -webkit-user-select: none;
-       -moz-user-select: none;
-        -ms-user-select: none;
-            user-select: none;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-}
-[effect=ripple]:before {
-    content: "";
-    display: block;
-    border-radius: 50%;
-    position: absolute;
-    pointer-events: none;
-    transform-origin: center;
-    top: calc(var(--y) * 1px);
-    left: calc(var(--x) * 1px);
-    width: calc(var(--d) * 1px);
-    height: calc(var(--d) * 1px);
-    background: var(--ripple-background, white);
-    transform: translate(-50%, -50%) scale(var(--s, 1));
-    opacity: calc(var(--o, 1) * var(--ripple-opacity, 0.3));
-    transition: calc(var(--t, 0) * var(--ripple-duration, 600ms)) var(--ripple-easing, linear);
-}
+// https://github.com/BaseMax/RippleEffectCSS
+document.querySelectorAll(`[effect="ripple"]`).forEach(el => {
+    el.addEventListener('click', e => {
+        e = e.touches ? e.touches[0] : e;
+        const r = el.getBoundingClientRect(),
+              d = Math.sqrt(Math.pow(r.width, 2) + Math.pow(r.height, 2)) * 2;
+        el.style.cssText = `--s: 0; --o: 1;`;
+        el.offsetTop;
+        el.style.cssText = `--t: 1; --o: 0; --d: ${d}; --x:${e.clientX - r.left}; --y:${e.clientY - r.top};`;
+    });
+});
+
+
