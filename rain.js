e5d32b726a020dda05c7a25b42896e8ec9d6f91d
JABSOL <119754639+JABSOL@users.noreply.github.com>
2024-01-28 17:26:57 +0100

    11

--- rain.js
+++ rain.js
@@ -1,35 +1,51 @@
-<script>
-        var canvas = document.getElementById('canvas');
-        var context = canvas.getContext('2d');
-        var W = window.innerWidth;
-        var H = window.innerHeight;
+// geting canvas by Boujjou Achraf
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
 
-        canvas.width = W;
-        canvas.height = H;
+//making the canvas full screen
+c.height = window.innerHeight;
+c.width = window.innerWidth;
 
-        var fontSize = 16;
-        var columns = Math.floor(W / fontSize);
-        var drops = [];
-        for(var i=0; i<columns; i++){
-            drops.push(0);
-        }
-        var str = "JavaScript Hacking Effect";
-        function draw(){
-            context.fillStyle = "rgba(0,0,0,0.05)";
-            context.fillRect(0, 0, W, H);
-            context.fontSize = "700 " + fontSize + "px";
-            context.fillStyle = "#00cc33";
-            for(var i=0; i<columns; i++){
-                var index = Math.floor(Math.random()*str.length);
-                var x = i * fontSize;
-                var y = drops[i] * fontSize;
-                context.fillText(str[index], x, y);
-                if(y >= canvas.height && Math.random() > 0.99){
-                    drops[i] = 0;
-                }
-                drops[i]++;
-            }
-        }
-        draw();
-        setInterval(draw, 35);
-    </script>
+//chinese characters - taken from the unicode charset
+var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+//converting the string into an array of single characters
+matrix = matrix.split("");
+
+var font_size = 10;
+var columns = c.width/font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+    drops[x] = 1; 
+
+//drawing the characters
+function draw()
+{
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctx.fillRect(0, 0, c.width, c.height);
+
+    ctx.fillStyle = "rgba(0, 100, 0, 0.1)";//green text
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for(var i = 0; i < drops.length; i++)
+    {
+        //a random chinese character to print
+        var text = matrix[Math.floor(Math.random()*matrix.length)];
+        //x = i*font_size, y = value of drops[i]*font_size
+        ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+        //sending the drop back to the top randomly after it has crossed the screen
+        //adding a randomness to the reset to make the drops scattered on the Y axis
+        if(drops[i]*font_size > c.height && Math.random() > 0.975)
+            drops[i] = 0;
+
+        //incrementing Y coordinate
+        drops[i]++;
+    }
+}
+
+setInterval(draw, 35);
